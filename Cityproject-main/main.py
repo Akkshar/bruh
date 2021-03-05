c0a62b2ecae71ce53efab1e9da2f865ec5557c4a
Akkshar <57217402+Akkshar@users.noreply.github.com>
2021-03-05 17:09:46 +0530

    Add files via upload

---
+++ Cityproject-main/main.py
@@ -0,0 +1,16 @@
+print("Hi! This is a program that will tell you the main cities in a continent")
+print("that you choose\nbased on: area, population")
+print("⚠️dont type in any typos please!⚠️")
+
+def cities():
+    continent = input("please any continent (Asia, Africa, Europe, North america, South america):-")
+    file = open(continent,'r')
+    num = 0
+    ans = 0
+    for var in file:
+        words = var.split('\n')
+        num = num+len(words)
+    print(words)
+    num = input("Do you want to know any information about any city (y/n)")
+    
+cities()
